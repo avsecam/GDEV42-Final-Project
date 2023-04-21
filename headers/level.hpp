03d7c4d4fde1e6b1f0339807910e737c50996034
Jed <jed.jocson@obf.ateneo.edu>
2023-04-21 21:19:19 +0800

    Add curve generation and apply Obstacle changes

--- headers/level.hpp
+++ headers/level.hpp
@@ -4,21 +4,26 @@
 #include <vector>
 
 #include "entity.hpp"
+#include "bezier.hpp"
 
 struct Level {
   Player* player;
-  std::vector<Entity*> staticObstacles;
-  std::vector<MovingEntity*> movingObstacles;
+  std::vector<Obstacle*> obstacles;
 
   void Draw() {
-    for (Entity* o : staticObstacles) {
-      o->Draw();
-    }
-    for (Entity* o : movingObstacles) {
+    for (Obstacle* o : obstacles) {
       o->Draw();
     }
     player->Draw();
   }
+
+	void GeneratePaths() {
+		for (Obstacle* o : obstacles) {
+			if (o->type == ObstacleType::MOVING) {
+				o->path.CalculateCurve();
+			}
+		}
+	}
 };
 
 Level* LoadLevel(const char filename[]) {
@@ -41,13 +46,14 @@ Level* LoadLevel(const char filename[]) {
 	int movingObstacleCount;
   levelFile >> staticObstacleCount;
   levelFile >> movingObstacleCount;
+	int highestCurveOrder = 0;
   for (int i = 0; i < staticObstacleCount; ++i) {
     Vector2 oPosition;
     Vector2 oHalfSizes;
     levelFile >> oPosition.x >> oPosition.y;
     levelFile >> oHalfSizes.x >> oHalfSizes.y;
-    Entity* o = new Entity(oPosition, oHalfSizes, OBSTACLE_COLOR);
-    level->staticObstacles.push_back(o);
+    Obstacle* o = new Obstacle(ObstacleType::STATIC, oPosition, oHalfSizes);
+    level->obstacles.push_back(o);
   }
 
 	for (int i = 0; i < movingObstacleCount; ++i) {
@@ -55,9 +61,36 @@ Level* LoadLevel(const char filename[]) {
     Vector2 oHalfSizes;
     levelFile >> oPosition.x >> oPosition.y;
     levelFile >> oHalfSizes.x >> oHalfSizes.y;
-    MovingEntity* o = new MovingEntity(oPosition, oHalfSizes, OBSTACLE_COLOR);
-    level->movingObstacles.push_back(o);
+
+		int oCurveOrder;
+		int oControlPointCount;
+		float speed;
+		BezierCurve oPath;
+		levelFile >> oCurveOrder >> oControlPointCount;
+
+		if (oCurveOrder <= 0) {
+			std::string errorMsg = std::string("Curve order must be a positive integer! Given curve order: ") + std::to_string(oCurveOrder);
+			throw std::invalid_argument(errorMsg);
+		}
+
+		if (oCurveOrder > highestCurveOrder) {
+			highestCurveOrder = oCurveOrder;
+		}
+
+		for (int j = 0; j < oControlPointCount; ++j) {
+			Vector2 controlPoint;
+			levelFile >> controlPoint.x >> controlPoint.y;
+			oPath.points.push_back(controlPoint);
+		}
+
+    Obstacle* o = new Obstacle(ObstacleType::MOVING, oPosition, oHalfSizes);
+		o->path = oPath;
+    level->obstacles.push_back(o);
   }
+
+	if (highestCurveOrder > 0) {
+		GeneratePascalsTriangle(highestCurveOrder);
+	}
 
   levelFile.close();
 
